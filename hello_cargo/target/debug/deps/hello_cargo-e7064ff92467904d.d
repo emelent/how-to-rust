0f77fbb6920de0aac5a4d1af1b993a78511f56b4
Roy <roy.motloutsi@gmail.com>
2023-11-15 17:38:13 +0200

    Remove binaries

--- hello_cargo/target/debug/deps/hello_cargo-e7064ff92467904d.d
+++
@@ -1,5 +0,0 @@
-/home/roy/Code/rust/how-to-rust/hello_cargo/target/debug/deps/hello_cargo-e7064ff92467904d: src/main.rs
-
-/home/roy/Code/rust/how-to-rust/hello_cargo/target/debug/deps/hello_cargo-e7064ff92467904d.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/roy/Code/rust/how-to-rust/hello_cargo/target/debug/hello_cargo: /home/roy/Code/rust/how-to-rust/hello_cargo/src/main.rs
